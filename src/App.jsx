15a516a03b442fa747b027170a4186508cfdb3cb
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-02-08 20:57:05 -0600

    Added Greeting component with props

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Greeting from './Greeting';
+
+function App() {
+    return (
+        <>
+            <div>
+                <Greeting />
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting(props) {
+    return <h1>Hello, {props.name}! Welcome to React </h1>;
+}
+
+export default Greeting;

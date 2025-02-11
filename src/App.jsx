1bd19fe7a24ce1ffc7f74bc8f33acaf9582ee3b5
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-02-10 18:43:13 -0600

    Added state with a counter in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Greeting from './Greeting';
+import { useState } from 'react';
 
 function App() {
+    // step1: define the state
+    const [count, setCount] = useState(0);
+
     return (
         <>
             <div>
-                <Greeting />
+                {/*step2: display state*/}
+                <h1>Counter: {count}</h1>
+                <button onClick={() => setCount(count + 1)}>Increase</button>
             </div>
         </>
     );
--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,5 +1,11 @@
+// A better approach is using React Fragments (<>...</>) to avoid unnecessary <div> elements in the DOM:
 function Greeting(props) {
-    return <h1>Hello, {props.name}! Welcome to React </h1>;
+    return (
+        <>
+            <h1>Hello, {props.name}! Welcome to React</h1>
+            <h1>Nice meeting you, {props.name}! Welcome to React</h1>
+        </>
+    );
 }
 
 export default Greeting;
